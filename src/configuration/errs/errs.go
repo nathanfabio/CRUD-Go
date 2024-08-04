92b8417729754b5dbd62c57dbd08e08bd58df8c4
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-08-04 18:24:13 -0300

    feat: add methods and functions necessary for errors

--- src/configuration/errs/errs.go
+++ src/configuration/errs/errs.go
@@ -1,5 +1,7 @@
 package errs
 
+import "net/http"
+
 type Errs struct {
 	Message string   `json:"message"`
 	Err     string   `json:"err"`
@@ -10,4 +12,65 @@ type Errs struct {
 type Causes struct {
 	Field   string `json:"field"`
 	Message string `json:"message"`
+}
+
+// Error returns the error message. When you need to pass an error as a parameter
+func (e *Errs) Error() string {
+	return e.Message
+}
+
+// NewErrs create a new error
+func NewErrs(message, err string, code int, causes []Causes) *Errs {
+	return &Errs {
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+//NewBadRequestErrs create a new bad request error
+func NewBadRequestErrs(message string) *Errs {
+	return &Errs {
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+//NewBadRequestValidationErrs create a new bad request validation error
+func NewBadRequestValidationErrs(message string, causes []Causes) *Errs {
+	return &Errs{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+//NewInternalServerErrs create a new internal server error
+func NewInternalServerErrs(message string) *Errs {
+	return &Errs {
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+//NewNotFoundErrs create a new not found error
+func NewNotFoundErrs(message string) *Errs {
+	return &Errs {
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+//NewForbiddenErrs create a new forbidden error
+func NewForbiddenErrs(message string) *Errs {
+	return &Errs {
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
 }
