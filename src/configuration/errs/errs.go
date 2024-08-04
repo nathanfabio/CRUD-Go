3a065951f7f7d1023a647325de496f071c9c0838
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-08-04 17:50:50 -0300

    feat: create error object

---
+++ src/configuration/errs/errs.go
@@ -0,0 +1,13 @@
+package errs
+
+type Errs struct {
+	Message string   `json:"message"`
+	Err     string   `json:"err"`
+	Code    int      `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
