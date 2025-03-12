0c4a8696428b6eb14ada2feafb6d681d908b17ee
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-12 11:15:42 -0300

    feat: add user management functions for deleting, finding, and updating users

---
+++ src/model/delete_user.go
@@ -0,0 +1,7 @@
+package model
+
+import "github.com/nathanfabio/CRUD-Go/src/configuration/errs"
+
+func (*userDomain) DeleteUser(string) *errs.Errs {
+	return nil
+}
---
+++ src/model/update_user.go
@@ -0,0 +1,7 @@
+package model
+
+import "github.com/nathanfabio/CRUD-Go/src/configuration/errs"
+
+func (*userDomain) UpdateUser(string, userDomain) *errs.Errs {
+	return nil
+}
