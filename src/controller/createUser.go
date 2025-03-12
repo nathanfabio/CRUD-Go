125ce625c640c67080525ad610b2b45a0202fe4b
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-12 09:13:24 -0300

    feat: integrate logger for user creation process with structured logging

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,27 +1,45 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nathanfabio/CRUD-Go/src/configuration/logger"
 	"github.com/nathanfabio/CRUD-Go/src/configuration/validation"
 	"github.com/nathanfabio/CRUD-Go/src/controller/model/request"
+	"github.com/nathanfabio/CRUD-Go/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "Create a new user"),
+	)
 	var userRequest request.UserRequest
 
 	//ShouldBindJSON binds the JSON passed in the request body to the struct.
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to marshal object: error=%s\n", err.Error())
-			//c.JSON returns a JSON response with the status code and the JSON passed in the response body.
-			errRest := validation.ValidateUserError(err)
-			c.JSON(errRest.Code, errRest)
-			return
+		logger.Error("Error trying to validate user info", err, 
+			zap.String("journey", "Create a new user"),
+	)
+		//c.JSON returns a JSON response with the status code and the JSON passed in the response body.
+		errRest := validation.ValidateUserError(err)
+		c.JSON(errRest.Code, errRest)
+		return
 
 	}
 
-	fmt.Println(userRequest)
+	
+	response := response.UserResponse {
+		ID: "test",
+		Email: userRequest.Email,
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+	}
+
+	logger.Info("User created successfully", 
+		zap.String("journey", "Create a new user"),
+)
+
+	c.JSON(http.StatusOK, response)
 }
