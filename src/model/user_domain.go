12900f31c4e4b0cd78eec706a33d882f401efd76
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-18 13:54:01 -0300

    feat: enhance UserDomainInterface with getter methods and password encryption

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -3,31 +3,48 @@ package model
 import (
 	"crypto/md5"
 	"encoding/hex"
-
-	"github.com/nathanfabio/CRUD-Go/src/configuration/errs"
 )
+
+type UserDomainInterface interface {
+	GetEmail() string
+	GetPassword() string
+	GetName() string
+	GetAge() int8
+
+	EncryptPassword()
+}
 
 func NewUserDomain(email, password, name string, age int8) UserDomainInterface {
 	return &userDomain{email, password, name, age}
 }
 
 type userDomain struct {
-	Email    string
-	Password string
-	Name     string
-	Age      int8
+	email    string
+	password string
+	name     string
+	age      int8
+}
+
+func (ud *userDomain) GetEmail() string {
+	return ud.email
+}
+
+func (ud *userDomain) GetPassword() string {
+	return ud.password
+}
+
+func (ud *userDomain) GetName() string {
+	return ud.name
+}
+
+func (ud *userDomain) GetAge() int8 {
+	return ud.age
 }
 
 func (ud *userDomain) EncryptPassword() {
 	hash := md5.New()
 	defer hash.Reset()
-	hash.Write([]byte(ud.Password))
-	ud.Password = hex.EncodeToString(hash.Sum(nil))
+	hash.Write([]byte(ud.password))
+	ud.password = hex.EncodeToString(hash.Sum(nil))
 }
 
-type UserDomainInterface interface {
-	CreateUser() *errs.Errs
-	UpdateUser(string, userDomain) *errs.Errs
-	FindUser(string) (*userDomain, *errs.Errs)
-	DeleteUser(string) *errs.Errs
-}
