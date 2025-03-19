9aeaa6c2fb190c61185409031004e7c33de8e14d
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-19 10:34:39 -0300

    feat: integrate UserControllerInterface into main application for improved user management

--- main.go
+++ main.go
@@ -6,20 +6,27 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/nathanfabio/CRUD-Go/src/configuration/logger"
+	"github.com/nathanfabio/CRUD-Go/src/controller"
 	"github.com/nathanfabio/CRUD-Go/src/controller/routes"
+	"github.com/nathanfabio/CRUD-Go/src/model/service"
 )
 
 func main() {
 	logger.Info("Starting the application...")
+
 	err := godotenv.Load()
 	if err != nil {
     log.Fatal("Error loading .env file")
 }
 
+	service := service.NewUserDomainService()
+	userController := controller.NewUserControllerInterface(service)
+
 	router:= gin.Default() //Create a new router. Could be gin.New(), but gin.New() initializes a router without any middleware. While
 	//gin.Default() initializes the router with logger and recovery middleware.
 
-	routes.InitRoutes(&router.RouterGroup)
+	routes.InitRoutes(&router.RouterGroup, userController)
+
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
