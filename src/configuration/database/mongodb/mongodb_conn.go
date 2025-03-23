e3757780f9e5040d725982c1fa417c992f654f2a
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-22 22:25:13 -0300

    feat: refactor MongoDB connection to use environment variables for configuration

--- src/configuration/database/mongodb/mongodb_conn.go
+++ src/configuration/database/mongodb/mongodb_conn.go
@@ -2,22 +2,35 @@ package mongodb
 
 import (
 	"context"
+	"os"
 
 	"github.com/nathanfabio/CRUD-Go/src/configuration/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func InitConnection() {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+var (
+	MONGODB_URL = "MONGODB_URL"
+	MONGODB_USER_DB = "MONGODB_USER_DB"
+)
+
+func NewMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
+	mongodb_uri := os.Getenv(MONGODB_URL)
+	mongodb_database := os.Getenv(MONGODB_USER_DB)
+	
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongodb_uri))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	logger.Info("Connect to database")
+	logger.Info("Connected to database")
+
+	return client.Database(mongodb_database), nil
+
+
+	
 }
