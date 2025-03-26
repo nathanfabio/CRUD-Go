18f3f2678213b12579bded27b9f56d804b370cac
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-26 12:08:51 -0300

    feat: enhance CreateUser method to use entity converter and improve logging

--- src/model/repository/create_user_repository.go
+++ src/model/repository/create_user_repository.go
@@ -7,6 +7,9 @@ import (
 	"github.com/nathanfabio/CRUD-Go/src/configuration/errs"
 	"github.com/nathanfabio/CRUD-Go/src/configuration/logger"
 	"github.com/nathanfabio/CRUD-Go/src/model"
+	"github.com/nathanfabio/CRUD-Go/src/model/repository/entity/converter"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
 )
 
 const (
@@ -14,23 +17,27 @@ const (
 )
 
 func (ur *userRepository) CreateUser(userDomain model.UserDomainInterface) (model.UserDomainInterface, *errs.Errs) {
-	logger.Info("Init createUser repository")
+	logger.Info("Init createUser repository", zap.String("jouney", "createUser"))
 
 	collection_name := os.Getenv(MONGODB_USER_COLLECTION)
 
 	collection := ur.databaseConnection.Collection(collection_name)
 
-	value, err := userDomain.GetJSONValue()
-	if err != nil {
-		return nil, errs.NewInternalServerErrs(err.Error())
-	}
+	value := converter.ConvertDomainToEntity(userDomain)
+	
 
 	result, err := collection.InsertOne(context.Background(), value)
 	if err != nil {
+		logger.Error("Error to create user", err, zap.String("journey", "createUser"))
 		return nil, errs.NewInternalServerErrs(err.Error())
 	}
 
-	userDomain.SetID(result.InsertedID.(string))
+	value.ID = result.InsertedID.(primitive.ObjectID)
 
-	return userDomain, nil
+	logger.Info("CreateUser repository executed successfully",
+		zap.String("userId", value.ID.Hex()),
+		zap.String("journey", "createUSer"),
+)
+
+	return converter.ConvertEntityToDomain(*value), nil
 }
