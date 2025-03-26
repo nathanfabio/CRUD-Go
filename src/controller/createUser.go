ac699b9f6a42e04ee568064837819a0dfaef0666
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-26 12:09:02 -0300

    feat: improve CreateUser method logging and handle service response

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -18,14 +18,14 @@ var (
 
 func (uc *userControllerInterface) CreateUser(c *gin.Context) {
 	logger.Info("Init CreateUser controller",
-		zap.String("journey", "Create a new user"),
+		zap.String("journey", "createUser"),
 	)
 	var userRequest request.UserRequest
 
 	//ShouldBindJSON binds the JSON passed in the request body to the struct.
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		logger.Error("Error trying to validate user info", err, 
-			zap.String("journey", "Create a new user"),
+			zap.String("journey", "createUser"),
 	)
 		//c.JSON returns a JSON response with the status code and the JSON passed in the response body.
 		errRest := validation.ValidateUserError(err)
@@ -38,16 +38,19 @@ func (uc *userControllerInterface) CreateUser(c *gin.Context) {
 	
 	
 
-	if err := uc.service.CreateUser(domain); err != nil {
+	domainResult, err := uc.service.CreateUser(domain)
+	if err != nil {
+		logger.Error("Error to call CreateUser service", err, zap.String("journey", "createUser"))
 		c.JSON(err.Code, err)
 		return
 	}
 
 	logger.Info("User created successfully", 
-		zap.String("journey", "Create a new user"),
+		zap.String("userId", domainResult.GetID()),
+		zap.String("journey", "createUser"),
 )
 
 	c.JSON(http.StatusOK, view.ConvertDomainToResponse(
-		domain,
+		domainResult,
 	))
 }
