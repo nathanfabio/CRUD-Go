4d6bbbda341d33238a7c96524283ebd76a6c7d99
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-26 12:08:16 -0300

    refactor: simplify userDomain struct by removing unused fields and methods

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,74 +1,34 @@
 package model
 
-import (
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-)
 
-type UserDomainInterface interface {
-	GetEmail() string
-	GetPassword() string
-	GetName() string
-	GetAge() int8
-
-	SetID(string)
-
-	GetJSONValue() (string, error)
-
-	EncryptPassword()
-}
-
-func NewUserDomain(email, password, name string, age int8) UserDomainInterface {
-	return &userDomain{
-		Email: email,
-		Password: password,
-		Name: name,
-		Age: age,
-	}
+type userDomain struct {
+	id       string
+	email    string
+	password string
+	name     string
+	age      int8
 }
 
 func (ud *userDomain) SetID(id string) {
-	ud.ID = id
-
+	ud.id = id
 }
 
-type userDomain struct {
-	ID       string
-	Email    string
-	Password string
-	Name     string
-	Age      int8
-}
-
-func (ud *userDomain) GetJSONValue() (string, error) {
-	b, err := json.Marshal(ud)
-	if err != nil {
-		return "", err
-	}
-
-	return string(b), nil
+func (ud *userDomain) GetID() string {
+	return ud.id
 }
 
 func (ud *userDomain) GetEmail() string {
-	return ud.Email
+	return ud.email
 }
 
 func (ud *userDomain) GetPassword() string {
-	return ud.Password
+	return ud.password
 }
 
 func (ud *userDomain) GetName() string {
-	return ud.Name
+	return ud.name
 }
 
 func (ud *userDomain) GetAge() int8 {
-	return ud.Age
-}
-
-func (ud *userDomain) EncryptPassword() {
-	hash := md5.New()
-	defer hash.Reset()
-	hash.Write([]byte(ud.Password))
-	ud.Password = hex.EncodeToString(hash.Sum(nil))
+	return ud.age
 }
